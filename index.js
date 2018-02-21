fde38d5b4e562ef3e3de71cbce8a98fd7df88af4
HugoCMDEV <36610870+HugoCMDEV@users.noreply.github.com>
2018-02-21 16:57:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-const Discord = require ('discord.js');
-
-var bot = new Discord.Client();const Discord = require ('discord.js');
-
-var bot = new Discord.Client();
-var prefix = ('^');
-
-bot.on('ready', () => {
-console.log("Bot Ready !");  
-
-bot.login('NDE1ODkzNzc3MTk0OTQyNDk0.DW8nKg.QRdreu5Gr4grMGQKqIbVZAv4nTc')
-
-bot.user.setPresence ({ game: { name: '^help in Forsythia', type: 0 } })
-    
-});
 
