e1ecb12f468c7a70cb07937405ccca4f8eb1203a
HugoCMDEV <36610870+HugoCMDEV@users.noreply.github.com>
2018-02-21 16:55:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,16 @@
 const Discord = require ('discord.js');
 
+var bot = new Discord.Client();const Discord = require ('discord.js');
+
 var bot = new Discord.Client();
 var prefix = ('^');
 
 bot.on('ready', () => {
-    bot.user.setPresence ({ game: { name: '^help in Forsythia', type: 0 } })
-    console.log("Bot Ready !");
+console.log("Bot Ready !");  
 
-});
+bot.login('NDE1ODkzNzc3MTk0OTQyNDk0.DW8nKg.QRdreu5Gr4grMGQKqIbVZAv4nTc')
 
-bot.login('NDE0NzM1NTc4NDE2OTM5MDIw.DWtxnw.UeLie9K8WYRszQkzg2TzWEiEVlk')
-
-bot.on('message' , message => {
-    if (message.content === prefix + "ping"){
-        message.channel.sendMessage("```pong```")
-        console.log('ping pong');
-    }
+bot.user.setPresence ({ game: { name: '^help in Forsythia', type: 0 } })
     
-    if (message.content === prefix + "help"){
-        var help_embed = new Discord.RichEmbed()
-                .setColor('#F49300')
-                .addField("__Commandes du Bot Forsythia__", "-")
-                .addField("^help", "Affiche les commandes du bot ")
-        message.channel.sendEmbed(help_embed);        
-        //message.channel.sendMessage("**Bienvenue dans l'interface des commandes du bot** \n**- f-help** ~ te permets de voir les commandes utilisables");
-        console.log("commande help demandée !");
-    }
 });
+
